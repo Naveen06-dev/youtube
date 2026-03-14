a87cb99f169f7ecf591de4a246094f3fbeb71402
Naveen06-dev <naveenkvg8@gmail.com>
2026-03-14 13:44:39 +0530

    updated design

--- utube_final/backend/app/main.py
+++ utube_final/backend/app/main.py
@@ -70,7 +70,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173", 
         "http://127.0.0.1:5173",
-        "https://youtube-eosin-tau.vercel.app"
+        "https://youtube-eosin-tau.vercel.app",
+        "https://realtime-ntube.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
